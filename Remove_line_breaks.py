36a1746adebcba22a29c4c99a2f9f75c9e53fade
symaeh <36413393+symaeh@users.noreply.github.com>
2018-07-14 22:07:52 +0800

    Update Remove_line_breaks.py

--- Remove_line_breaks.py
+++ Remove_line_breaks.py
@@ -6,13 +6,13 @@ copy_text = wc.GetClipboardData(win32con.CF_TEXT)
 wc.CloseClipboard()
 #print(copy_text,type(copy_text))
 
-copy_text = copy_text.decode("gb2312")
+copy_text = copy_text.decode(encoding="utf-8", errors="ignore")
 #print(copy_text,type(copy_text))
 
 copy_text = copy_text.replace('\r\n',' ')
 #print(copy_text,type(copy_text))
 
-copy_text = copy_text.encode(encoding="gb2312")
+copy_text = copy_text.encode(encoding="utf-8")
 
 wc.OpenClipboard()
 wc.EmptyClipboard()
