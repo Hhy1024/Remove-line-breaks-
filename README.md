04753dff4d7f85b54c9949178c9e6ed01635c5f3
symaeh <zwfsuzhou@qq.com>
2018-11-01 11:30:49 +0800

    18.11.1 Directly use methods for Bytes class.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ As a novice, when reading pdfs of a new domain, we often copy some sentences to 
 Method: firstly, copy (ctrl+c) content, and then run Remove_line_breaks.py. Finally paste(ctrl+v) in the translation page.
 
 作为一个新手，看新领域文献时常常要复制内容到翻译网站，网站不会自动把pdf内换行符去掉，导致每句话不完整，翻译不准确。而这个小程序解决了这个问题，使用方法：首先复制（ctrl+c）内容，然后运行Remove_line_breaks.py，最后在翻译网页内粘贴(ctrl+v)即可。
+
+- 11.1改动，改成直接操作字节。
--- Remove_line_breaks.py
+++ Remove_line_breaks.py
@@ -4,15 +4,10 @@ import win32con
 wc.OpenClipboard()
 copy_text = wc.GetClipboardData(win32con.CF_TEXT)
 wc.CloseClipboard()
-#print(copy_text,type(copy_text))
 
-copy_text = copy_text.decode(encoding="utf-8", errors="ignore")
-#print(copy_text,type(copy_text))
-
-copy_text = copy_text.replace('\r\n',' ')
-#print(copy_text,type(copy_text))
-
-copy_text = copy_text.encode(encoding="utf-8")
+#print(copy_text,type(copy_text),'\n')
+copy_text = copy_text.replace(b'\r\n',b' ')
+#print(copy_text,type(copy_text),'\n')
 
 wc.OpenClipboard()
 wc.EmptyClipboard()
